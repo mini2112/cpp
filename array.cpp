46eef02894867ae3a9fcd0fcbc711d83c381714c
Prerna <patnayakprerna@gmail.com>
2023-03-28 15:41:35 +0530

    initial github

---
+++ array.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+    
+    int A[5]={2,4,5,6,7};
+    
+    cout<<"size of the array"<<endl;
+    cout<<A[2]<<endl;
+    cout<<sizeof(A)<<endl;
+
+
+    return 0;
+
+}
---
+++ helloworld.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"this is prerna."<<endl;
+    cout<<"enter a number";
+    int a;
+    cin>>a;
+    return 0;
+}

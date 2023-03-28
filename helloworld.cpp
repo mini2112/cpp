a1cb95803cf0fa44d17a09429880134c06830855
Prerna <patnayakprerna@gmail.com>
2023-03-28 15:52:18 +0530

    entered a cout

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 int main(){
     cout<<"this is prerna."<<endl;
-    cout<<"enter a number";
+    cout<<"enter a number"<<endl;
+    cout<<"entered a new line for github"<<endl;
     int a;
     cin>>a;
     return 0;

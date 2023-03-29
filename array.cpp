0edabf2f1091bd7c3870595c4f09ba0c955447f5
Prerna <patnayakprerna@gmail.com>
2023-03-29 15:44:01 +0530

    array

--- array.cpp
+++ array.cpp
@@ -4,11 +4,15 @@ using namespace std;
 
 int main (){
     
-    int A[5]={2,4,5,6,7};
+    // int A[5]={2,4,5,6,7};
     
-    cout<<"size of the array"<<endl;
-    cout<<A[2]<<endl;
-    cout<<sizeof(A)<<endl;
+    // cout<<"size of the array"<<endl;
+    // cout<<A[2]<<endl;
+    // cout<<sizeof(A)<<endl;
+
+    int arr[5];
+      cin>>arr[0]>>arr[1]>>arr[2]>>arr[3]>>arr[4];
+      cout<<arr[3];
 
 
     return 0;

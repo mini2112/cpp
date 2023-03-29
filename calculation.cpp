470f80f9a9d2a14b8309c4f7794006d7d142fda6
aman yadav <aman2002official@gmail.com>
2023-03-29 14:46:28 +0530

    added input in the calculation.cpp file

--- calculation.cpp
+++ calculation.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 using namespace std;
-int main(){
-    int a = 2;
-    int b = 5;
-    cout<<"multiplication of these numbers are "<<a*b<<endl;
+int main()
+{
+    // int a = 2;
+    // int b = 5;
+    // changing code here making input for two numbers
+    int a, b;
+    cout << "enter two number to  get multiplication of them:" << endl;
+    cin >> a >> b;
+    cout << "multiplication of these numbers are " << a * b << endl;
     return 0;
 }

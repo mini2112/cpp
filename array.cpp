9cd7fa4591c26d852232b3ff15874becd0463f56
Prerna <patnayakprerna@gmail.com>
2023-04-04 19:29:24 +0530

    array

--- array.cpp
+++ array.cpp
@@ -10,9 +10,12 @@ int main (){
     // cout<<A[2]<<endl;
     // cout<<sizeof(A)<<endl;
 
-    int arr[5];
+    /*int arr[5];
       cin>>arr[0]>>arr[1]>>arr[2]>>arr[3]>>arr[4];
-      cout<<arr[3];
+
+      arr[3] = 10;
+      cout<<arr[3];*/
+      
 
 
     return 0;

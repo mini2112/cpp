6eec0692888419031f3690578f9331c5b6b78925
aman yadav <aman2002official@gmail.com>
2023-04-05 10:54:16 +0530

    updated the sum function

--- function.cpp
+++ function.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
 using namespace std;
 
-//Take two numbers and print its sum
+// Take two numbers and print its sum
 
-int SUm(int a,int b ){
-   int  SUM=a+b;
-   return SUM;
-   
-    
+int SUm(int a, int b)
+{
+    //    int  SUM=a+b;
+    // directly return the sum
+    return a + b;
 }
-int main(){
-    int a,b;
-    cin>>a>>b;
-   int res= SUm(a,b);
-    cout<<"the sum of these number is "<<res<<endl;
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    int res = SUm(a, b);
+    cout << "the sum of these number is " << res << endl;
 
     return 0;
 }
